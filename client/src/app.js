e23866791d56ab6172821e65dafe3bde8a2a872b
emerson <69053988+Jed2020@users.noreply.github.com>
2021-11-01 03:57:29 -0300

    instalação react router dom e import /uso no app.js

--- client/src/app.js
+++ client/src/app.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+
 import Form from './pages/form';
 import MyForm from './pages/login';
 import "./App.css";
 
-function App() {
+function App() {  
   return (
+    <Router>
     <div className="App">
-      <Form />
-      <MyForm />
+      <Route>
+      <Link to="./api/insert">< Form /></Link>
+      </Route>
+      <Route>
+      <Link to="./api/select"><MyForm /></Link>
+      </Route>      
     </div>
+    </Router>
   );
 }
 
