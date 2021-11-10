ca0a4bc46cb1beb72d79372e68463cb9380d829b
emerson <69053988+Jed2020@users.noreply.github.com>
2021-11-10 16:13:20 -0300

    uso de yup no register.js no client

--- client/src/components/register/register.js
+++ client/src/components/register/register.js
@@ -5,8 +5,9 @@ import{
 } from "@material-ui/core";
 import "../form.css";
 import Axios from 'axios';
-
-
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
 
 function Form() {
     const [cpf, setCpf] = useState('');
@@ -14,34 +15,58 @@ function Form() {
     const [cargo, setCargo] = useState('');
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
-    
-    const [emailError, setEmailError] = useState(false);
 
+    
+
+    const validate = yup.object().shape({
+      cpf: yup.number()
+      .required()
+      .min(10)
+      .max(11)
+      .positive().integer(),
+      cargo: yup.string()
+        .required(),
+      senha: yup.string()
+        .required()
+        .min(6),
+      email: yup.string()
+        .required()
+        .email(),
+      nome: yup.string()
+        .required(),
+    });
+
+    const { register, handleSubmit} = useForm({
+      resolver: yupResolver(validate),      
+    });
+    
+    
     const submitReview = () => {
       Axios.post("http://localhost:3001/api/insert", {
         cpf: cpf, nome: nome, cargo: cargo, email: email, senha: senha,
       }).then(() => {
         alert('Cadastro realizado com Sucesso!')
       });
-    };
-  
+    };   
+      
     return (
       <div className="form">
         <h1>Faça seu de Cadastro</h1>
-        <form onSubmit={(event) => {
-          event.preventDefault();
-        }}>
+        <form onSubmit={handleSubmit(submitReview)}>
           <TextField
             id="cpf"
+            name="cpf"
             label="CPF"
             variant="outlined"
             margin="dense"
-            fullWidth
+            fullWidth            
+            {...register('cpf', { required: true })}            
             value={cpf}
             onChange={(event) => {setCpf(event.target.value)}}
           />
           <TextField
             id="nome"
+            name="nome"
             label="Nome Completo"
             variant="outlined"
             margin="dense"
@@ -51,43 +76,27 @@ function Form() {
           />
           <TextField
             id="cargo"
+            name="cargo"
             label="Cargo na Empresa"
             variant="outlined"
             margin="dense"
-            fullWidth
+            fullWidth          
             value={cargo}
             onChange={(event) => {setCargo(event.target.value)}}
           />
           <TextField
             id="email"
+            name="email"
             label="E-mail"
             variant="outlined"
             margin="dense"
-            fullWidth
-            error={emailError}
-            helperText={emailError && "Deve conter @."}
+            fullWidth      
             value={email}
-            onBlur={(event) => {
-              const textoEmail = event.target.value;
-  
-              if (textoEmail.length === '@') {
-                setEmailError(true);
-              } else {
-                setEmailError(false);
-              }
-            }}
-            onChange={(event) => {
-              const textoEmail = event.target.value;
-  
-              if (textoEmail.length !== '@') {
-                setEmailError(false);
-              }
-  
-              setEmail(event.target.value)}
-            }
+            onChange={(event) => {setEmail(event.target.value)}}
           />
           <TextField
             id="senha"
+            name="senha"
             label="Senha"
             variant="outlined"
             margin="dense"
@@ -98,7 +107,7 @@ function Form() {
           <Button onClick={submitReview} className="btn-form" variant="contained" color="primary">
             Cadastrar
           </Button>
-        </form>  
+        </form>
       </div>
     );
   }
