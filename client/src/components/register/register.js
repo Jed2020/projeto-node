f30d440d30725b773736f0e8c6dde452b807939f
emerson <69053988+Jed2020@users.noreply.github.com>
2021-11-10 22:32:29 -0300

    atualização no uso do yup em register.js

--- client/src/components/register/register.js
+++ client/src/components/register/register.js
@@ -1,115 +1,115 @@
-import React, { useState } from "react";
-import{
-    Button,
-    TextField,
-} from "@material-ui/core";
-import "../form.css";
+import React from "react";
+import { TextField, Button, Box } from "@material-ui/core";
+import { Field, Form, Formik } from "formik";
+import { object, string } from "yup";
 import Axios from 'axios';
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { useForm } from "react-hook-form";
 
-function Form() {
-    const [cpf, setCpf] = useState('');
-    const [nome, setNome] = useState('');
-    const [cargo, setCargo] = useState('');
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
+const initalValues = {
+  cpf: "",
+  nome: "",
+  cargo: "", 
+  email: "",
+  senha: "",
+};
 
-    
+const FormRegister = () => {
+  return (
+    <div className="form">
+      <h1>Faça seu de Cadastro</h1>
+      <Formik
+        initialValues={initalValues}
+        validationSchema={object({
+          cpf: string().required().max(11, "Máximo de 11 dígitos"),
+          email: string().required("Por favot digite o email").email("E-mail invalido"),
+          cargo: string().required("Por favot digite o cargo").min(2, "Cargo muito curto"),
+          nome: string().required("Por favot digite o nome").min(2, "Nome muito curto"),
+          senha: string().required("Por favot digite a senha").min(6, "Senha de no mínimo 6 caracteres"),
+        })}
+        onSubmit={(values, formikHelpers) => {
+          Axios.post("http://localhost:3001/api/insert", {
+          cpf: values, nome: values, cargo: values, email: values, senha: values,});
+          console.log(values);
+          formikHelpers.resetForm();
+        }}
+      >
+        {({ errors, isValid, touched, dirty }) => (
+          <Form>
+            <Field
+              name="cpf"
+              type="cpf"
+              as={TextField}
+              variant="outlined"
+              color="primary"
+              label="CPF"
+              fullWidth
+              error={Boolean(errors.cpf) && Boolean(touched.cpf)}
+              helperText={Boolean(touched.cpf) && errors.cpf}
+            />
+            <Box height={14} />
+            <Field
+              name="nome"
+              type="nome"
+              as={TextField}
+              variant="outlined"
+              color="primary"
+              label="Nome"
+              fullWidth
+              error={Boolean(errors.nome) && Boolean(touched.nome)}
+              helperText={Boolean(touched.nome) && errors.nome}
+            />
+            <Box height={14} />
+            <Field
+              name="cargo"
+              type="cargo"
+              as={TextField}
+              variant="outlined"
+              color="primary"
+              label="Cargo"
+              fullWidth
+              error={Boolean(errors.cargo) && Boolean(touched.cargo)}
+              helperText={Boolean(touched.cargo) && errors.cargo}
+            />
+            <Box height={14} />
+            <Field
+              name="email"
+              type="email"
+              as={TextField}
+              variant="outlined"
+              color="primary"
+              label="E-mail"
+              fullWidth
+              error={Boolean(errors.email) && Boolean(touched.email)}
+              helperText={Boolean(touched.email) && errors.email}
+            />
+            <Box height={14} />
+            <Field
+              name="senha"
+              type="senha"
+              as={TextField}
+              variant="outlined"
+              color="primary"
+              label="Senha mínimo 6 caracteres"
+              fullWidth
+              error={Boolean(errors.senha) && Boolean(touched.senha)}
+              helperText={Boolean(touched.senha) && errors.senha}
+            />
+            <Box height={14} />
 
-    const validate = yup.object().shape({
-      cpf: yup.number()
-      .required()
-      .min(10)
-      .max(11)
-      .positive().integer(),
-      cargo: yup.string()
-        .required(),
-      senha: yup.string()
-        .required()
-        .min(6),
-      email: yup.string()
-        .required()
-        .email(),
-      nome: yup.string()
-        .required(),
-    });
+            <Button
+              type="submit"
+              variant="contained"
+              onClick={submitReview}
+              color="primary"
+              size="large"
+              disabled={!isValid || !dirty}
+            >
+              Cadastrar
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
 
-    const { register, handleSubmit} = useForm({
-      resolver: yupResolver(validate),      
-    });
-    
-    
-    const submitReview = () => {
-      Axios.post("http://localhost:3001/api/insert", {
-        cpf: cpf, nome: nome, cargo: cargo, email: email, senha: senha,
-      }).then(() => {
-        alert('Cadastro realizado com Sucesso!')
-      });
-    };   
-      
-    return (
-      <div className="form">
-        <h1>Faça seu de Cadastro</h1>
-        <form onSubmit={handleSubmit(submitReview)}>
-          <TextField
-            id="cpf"
-            name="cpf"
-            label="CPF"
-            variant="outlined"
-            margin="dense"
-            fullWidth            
-            {...register('cpf', { required: true })}            
-            value={cpf}
-            onChange={(event) => {setCpf(event.target.value)}}
-          />
-          <TextField
-            id="nome"
-            name="nome"
-            label="Nome Completo"
-            variant="outlined"
-            margin="dense"
-            fullWidth
-            value={nome}
-            onChange={(event) => {setNome(event.target.value)}}
-          />
-          <TextField
-            id="cargo"
-            name="cargo"
-            label="Cargo na Empresa"
-            variant="outlined"
-            margin="dense"
-            fullWidth          
-            value={cargo}
-            onChange={(event) => {setCargo(event.target.value)}}
-          />
-          <TextField
-            id="email"
-            name="email"
-            label="E-mail"
-            variant="outlined"
-            margin="dense"
-            fullWidth      
-            value={email}
-            onChange={(event) => {setEmail(event.target.value)}}
-          />
-          <TextField
-            id="senha"
-            name="senha"
-            label="Senha"
-            variant="outlined"
-            margin="dense"
-            fullWidth
-            value={senha}
-            onChange={(event) => {setSenha(event.target.value)}}
-          />
-          <Button onClick={submitReview} className="btn-form" variant="contained" color="primary">
-            Cadastrar
-          </Button>
-        </form>
-      </div>
-    );
-  }
-  
-  export default Form;
+export default FormRegister;
