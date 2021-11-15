1def6cf4d68b4470ab603f76c191ff40a1480e35
emerson <69053988+Jed2020@users.noreply.github.com>
2021-11-15 16:47:15 -0300

    headerCurriculum.js na page2.js

---
+++ client/src/components/header/headerCurriculum.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import AppBar from './appBar';
+import Toolbar from './toolBar';
+
+
+const rightLink = {
+  fontSize: 16,
+  color: 'common.white',
+  ml: 3,
+};
+
+function AppAppBar() {
+  return (
+    <div>
+      <AppBar position="fixed" style={{ background: '#28282a'}}>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <Box sx={{ flex: 1 }} />
+          <Link
+            variant="h6"
+            underline="none"
+            color="inherit"
+            href="/"
+            sx={{ fontSize: 24 }}
+          >
+            {'DevDotCom'}
+          </Link>
+          <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+            <Link
+              color="inherit"
+              variant="h6"
+              underline="none"
+              sx={rightLink}
+            >
+              {'Relatório'}
+            </Link>
+            <Link
+              variant="h6"
+              underline="none"
+              href="#cadastro"  
+              sx={{ ...rightLink}}
+            >
+              {'Editar'}
+            </Link>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </div>
+  );
+}
+
+export default AppAppBar;
