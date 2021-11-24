b702dc6be376afbbf5ca724267d525dc1549185f
emerson <69053988+Jed2020@users.noreply.github.com>
2021-11-24 17:22:37 -0300

    criação pageNotFound.js em pages

---
+++ client/src/pages/pageNotFound.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Typography from '../components/body/typography';
+
+function PageNotFound() {
+  return (
+    <div className="pageNotFound">
+     <Typography variant="h4" marked="center" align="center" component="h2">
+      Oops..! 404 Page Not Found
+      </Typography>
+      <Typography variant="h4" marked="center" align="center" component="h2">
+      Looks like you came to wrong page on our server
+     </Typography>
+     <Typography variant="h4" marked="center" align="center" component="h2">
+     <img src={'https://raw.githubusercontent.com/oshyam/404-with-react-router-dom/master/src/pagenotfound.jpg'} height="500" width="500" alt="not found"/>
+     </Typography>
+    </div>
+  );
+}
+
+export default PageNotFound;    
--- client/src/route.js
+++ client/src/route.js
@@ -7,26 +7,20 @@ import Page3 from "./pages/page3";
 import Page4 from "./pages/page4";
 import Page5 from "./pages/page5";
 import Page6 from "./pages/page6";
-
+import PageNotFound from "./pages/pageNotFound";
 
 function Routes() {  
   return (
     <Router>
       <Switch>
-      <Route component={Main} exact path="/">
-      </Route>
-      <Route component={Page1} exact path="/inicial">
-      </Route>
-      <Route component={Page2} exact path="/curriculum">
-      </Route>
-      <Route component={Page3} exact path="/table">
-      </Route>
-      <Route component={Page4} exact path="/editableCurriculum">
-      </Route>
-      <Route component={Page5} exact path="/editableSchool">
-      </Route>
-      <Route component={Page6} exact path="/editableUser">
-      </Route>
+      <Route component={Main} exact path="/"/>      
+      <Route component={Page1} exact path="/inicial"/>      
+      <Route component={Page2} exact path="/curriculum"/>
+      <Route component={Page3} exact path="/table"/>
+      <Route component={Page4} exact path="/editableCurriculum"/>
+      <Route component={Page5} exact path="/editableSchool"/>
+      <Route component={Page6} exact path="/editableUser"/>
+      <Route path="*" exact component={PageNotFound}/>
       </Switch>      
     </Router>
   );
